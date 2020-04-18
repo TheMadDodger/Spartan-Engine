17a5bbb96a196b7cd1619be832e3c46471dc3c06
Robin <robin.cordemans302@gmail.com>
2020-04-18 14:09:14 +0100

    Forgot UIComponent files

---
+++ SpartanFramework/UIComponent.cpp
@@ -0,0 +1,2 @@
+#include "stdafx.h"
+#include "UIComponent.h"
---
+++ SpartanFramework/UIComponent.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "BaseComponent.h"
+class UIComponent : public BaseComponent
+{
+public:
+	UIComponent(const std::string& name);
+	UIComponent();
+};
